e03d2f2450cb185e49210906e7bddac23e7009b4
ps5go <147273768+ps5go@users.noreply.github.com>
2023-10-28 19:06:31 +0800

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -1,52 +1,51 @@
 const payload_map =
     [
-	    {
-            displayTitle: '一键折腾 (仅4.03)',
-            description: '允许解密/运行fpkg应用程序',
-            fileName: 'ps5-kstuff.bin',
-            author: 'sleirsgoevy',
-            source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
-            version: '1.0'
-        },
         {
-            displayTitle: '一键折腾V1.1（4.03-4.50）',
-            description: '允许解密/运行fpkg应用程序',
+            displayTitle: 'HEN',
+            description: 'FW 4.03 & 4.50 Only. FPKG enabler',
             fileName: 'ps5-kstuff-v1.1.bin',
             author: 'sleirsgoevy',
             source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
             version: '1.1'
         },
         {
-            displayTitle: 'ELF加载器',
-            description: '端口9021',
-            fileName: 'elfldr.elf',
-            author: 'john-tornblom',
-            source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
-            version: '0.5'
-        },
-        {
-            displayTitle: 'FTPS5 (非持久性)',
-            description: 'FTP服务',
-            fileName: 'ftps5-np.elf',
-            author: 'SiSTR0, zecoxao, EchoStretch',
-            source:'https://github.com/EchoStretch/FTPS5/releases',
-            version: '1.4'
-        },
-        {
-            displayTitle: 'FTPS5 (持久性)',
-            description: 'FTP服务',
+            displayTitle: 'FTP Server',
+            description: 'FTP Server port 1337',
             fileName: 'ftps5-p.elf',
             author: 'SiSTR0, zecoxao, EchoStretch',
             source:'https://github.com/EchoStretch/FTPS5/releases',
             version: '1.4'
         },
         {
-            displayTitle: '清除浏览器缓存',
-            description: 'PS5系统设置无法清除缓存，可以通过本插件清理',
-            fileName: 'cache_remove.elf',
+            displayTitle: 'PS5 Payload ELF Loader',
+            description: 'Uses port 9021. Persistent network elf loader',
+            fileName: 'elfldr.elf',
+            author: 'john-tornblom',
+            source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
+            version: '0.5'
+        },
+        {
+            displayTitle: 'Versions',
+            description: 'Shows kernel build, os and sdk versions',
+            fileName: 'versions.elf',
+            author: '?',
+            source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
+            version: '1.0'
+        },
+        {
+            displayTitle: 'TO99999999',
+            description: 'Go to version 99999999 of the FW.',
+            fileName: 'TO99999999.elf',
+            author: '@jaf51744',
+            source:'?',
+            version: '?'
+        },
+        {
+            displayTitle: 'Browser_Cache_remove',
+            description: 'Removes appcache from browser.',
+            fileName: 'Cache_remove.elf',
             author: 'Storm21CH',
             source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
             version: '1.0fix'
         }
-
     ];
