84465448de9e7316461edd001a3f6cb347721cd5
ps5go <147273768+ps5go@users.noreply.github.com>
2025-05-05 14:29:21 +0800

    Add files via upload

--- custom_host_stuff.js
+++ custom_host_stuff.js
@@ -28,9 +28,9 @@ async function run(wkonly = false, animate = true) {
         await run_psfree(fw_str);
 
     } catch (error) {
-        log("Webkit exploit failed: " + error, LogLevel.ERROR);
+        log("\u201cWebkit\u6f0f\u6d1e\u5229\u7528\u5931\u8d25: " + error, LogLevel.ERROR);
 
-        log("Retrying in 2 seconds...", LogLevel.LOG);
+        log("2\u79d2\u540e\u91cd\u8bd5...", LogLevel.LOG);
         await new Promise((resolve) => setTimeout(resolve, 2000));
         window.location.reload();
         return; // this is necessary
@@ -39,11 +39,11 @@ async function run(wkonly = false, animate = true) {
     try {
         await main(window.p, wkonly); // if all goes well, this should block forever
     } catch (error) {
-        log("Kernel exploit/main() failed: " + error, LogLevel.ERROR);
+        log("\u5185\u6838\u6f0f\u6d1e\u5229\u7528\u5931\u8d25\uff1a: " + error, LogLevel.ERROR);
         // p.write8(new int64(0,0), 0); // crash
     }
 
-    log("Retrying in 4 seconds...", LogLevel.LOG);
+    log("4\u79d2\u540e\u91cd\u8bd5...", LogLevel.LOG);
     await new Promise((resolve) => setTimeout(resolve, 4000));
     window.location.reload();
 }
@@ -128,55 +128,55 @@ function registerAppCacheEventHandlers() {
             createOrUpdateAppCacheToast('Offline.', 2000);
         } else {
             // this is redundant
-            createOrUpdateAppCacheToast("Checking for updates...");
+            createOrUpdateAppCacheToast("\u68c0\u67e5\u53ef\u7528\u66f4\u65b0...");
         }
     }
 
     appCache.addEventListener('cached', function (e) {
-        createOrUpdateAppCacheToast('Finished caching site.', 1500);
+        createOrUpdateAppCacheToast('\u7f13\u5b58\u5b8c\u6210.', 1500);
     }, false);
 
     appCache.addEventListener('checking', function (e) {
-        createOrUpdateAppCacheToast('Checking for updates...');
+        createOrUpdateAppCacheToast('\u68c0\u67e5\u5347\u7ea7\u6587\u4ef6...');
     }, false);
 
     appCache.addEventListener('downloading', function (e) {
-        createOrUpdateAppCacheToast('Downloading new cache...');
+        createOrUpdateAppCacheToast('\u4e0b\u8f7d\u6587\u4ef6...');
     }, false);
 
     appCache.addEventListener('error', function (e) {
         // only show error toast if we're online
         if (navigator.onLine) {
-            createOrUpdateAppCacheToast('Error while caching site.', 5000);
+            createOrUpdateAppCacheToast('\u7f13\u5b58\u7ad9\u70b9\u65f6\u51fa\u9519\u3002.', 5000);
         } else {
-            createOrUpdateAppCacheToast('Offline.', 2000);
+            createOrUpdateAppCacheToast('\u79bb\u7ebf\u6a21\u5f0f.', 2000);
         }
     }, false);
 
     appCache.addEventListener('noupdate', function (e) {
-        createOrUpdateAppCacheToast('Cache is up-to-date.', 1500);
+        createOrUpdateAppCacheToast('\u7f13\u5b58\u662f\u6700\u65b0\u7684.', 1500);
     }, false);
 
     appCache.addEventListener('obsolete', function (e) {
-        createOrUpdateAppCacheToast('Site is obsolete.');
+        createOrUpdateAppCacheToast('\u7f51\u7ad9\u5df2\u8fc7\u65f6.');
     }, false);
 
     appCache.addEventListener('progress', function (e) {
         let percentage = Math.round((e.loaded / e.total) * 100);
 
-        createOrUpdateAppCacheToast('Downloading new cache... ' + percentage + '%');
+        createOrUpdateAppCacheToast('\u4e0b\u8f7d\u8fdb\u5ea6... ' + percentage + '%');
 
         // the last item takes an unreasonably long time to complete (with a big update)
         // ig its doing some extra stuff before the last event is fired
         // so show a new message for it
         if (e.loaded + 1 == e.total) {
-            createOrUpdateAppCacheToast("Processing... This may take a minute.");
+            createOrUpdateAppCacheToast("\u5904\u7406\u4e2d...\u8bf7\u7b49\u5f85.");
         }
     }, false);
 
     appCache.addEventListener('updateready', function (e) {
         if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-            createOrUpdateAppCacheToast('The site was updated. Refresh to switch to updated version');
+            createOrUpdateAppCacheToast('\u7f51\u9875\u5df2\u66f4\u65b0\uff0c\u9000\u51fa\u5237\u65b0\u4ee5\u5207\u6362\u5230\u6700\u65b0\u7248\u672c');
         }
     }, false);
 }
@@ -185,7 +185,7 @@ function registerL2ButtonHandler() {
     document.addEventListener("keydown", async (event) => {
         if (event.keyCode === 118) {
             const lastRedirectorValue = localStorage.getItem(LOCALSTORE_REDIRECTOR_LAST_URL_KEY) || "http://";
-            const redirectorValue = prompt("Enter url", lastRedirectorValue);
+            const redirectorValue = prompt("\u8f93\u5165\u5730\u5740", lastRedirectorValue);
 
             // pressing cancel works as expected, but pressing the back button unfortunately is the same as pressing ok
             if (redirectorValue && redirectorValue !== "http://") {
