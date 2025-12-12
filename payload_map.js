1be12e938c0420a272839f93eee3f45f519eaf5b
ps5go <147273768+ps5go@users.noreply.github.com>
2025-12-12 22:59:17 +0800

    Update payload_map.js

--- payload_map.js
+++ payload_map.js
@@ -48,7 +48,7 @@ const payload_map = [
         projectSource: "https://github.com/EchoStretch/kstuff",
         binarySource: "https://github.com/EchoStretch/kstuff/actions/runs/14686953552",
         version: "1.6.6",
-        supportedFirmwares: ["2.", "3.", "4.", "5."],
+        supportedFirmwares: ["3.", "4.", "5."],
         toPort: 9021
     },
     {
@@ -108,6 +108,7 @@ const payload_map = [
     }
 
 ];
+
 
 
 
